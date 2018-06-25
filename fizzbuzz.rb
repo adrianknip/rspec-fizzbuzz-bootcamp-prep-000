a0324c8d5815a629c5223388056f52c7ed9fb369
nip.adrian@gmail.com Nip <nip.adrian@gmail.com>
2018-06-25 00:04:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,11 @@
 def fizzbuzz(x)
   if x % 3 == 0 
     return "Fizz"
-  else x % 5 == 0
+  end
+  if x % 5 == 0
     return "Buzz"
-  else x % 3 == 0 && x % 5 == 0
-    return "FizzBuzz"
+  end
+  # else x % 3 == 0 && x % 5 == 0
+  #   return "FizzBuzz"
   # else x % 3 != 0
 end  
